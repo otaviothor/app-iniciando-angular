031882efdb6e433c82b5dc6f45c6b4c3935dc91b
otavio <otaviosilva2632@gmail.com>
2022-02-19 23:28:24 -0300

    feat: create first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimeiroComponentComponent } from './primeiro-component/primeiro-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponentComponent
   ],
   imports: [
     BrowserModule
