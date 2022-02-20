c863bd02256a9cd359bdf086fa5be09efda09664
otavio <otaviosilva2632@gmail.com>
2022-02-20 17:03:40 -0300

    feat: working w/ services

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../services/courses/courses.service';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.scss']
+  styleUrls: ['./cursos.component.scss'],
 })
 export class CursosComponent implements OnInit {
+  public portalName: string = '';
+  public courses: string[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private coursesService: CoursesService) {
+    this.portalName = 'https://loiane.training';
   }
 
+  ngOnInit(): void {
+    this.courses = this.coursesService.getCourses();
+  }
 }
---
+++ src/app/services/courses/courses.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  constructor() {}
+
+  getCourses(): string[] {
+    return ['Java', 'Angular', 'React'];
+  }
+}
