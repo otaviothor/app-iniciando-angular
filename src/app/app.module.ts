0661eab42e9bc73bfc4a7d4853f4bd653cf51b07
otavio <otaviosilva2632@gmail.com>
2022-02-19 23:55:34 -0300

    feat: create courses module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { PrimeiroComponentComponent } from './primeiro-component/primeiro-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PrimeiroComponentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PrimeiroComponentComponent],
+  imports: [BrowserModule, CursosModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
