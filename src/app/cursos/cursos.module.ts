3c8b5afe01e534637939f33d0bcb57f968cfea25
otavio <otaviosilva2632@gmail.com>
2022-02-19 23:56:09 -0300

    feat: create courses-details component

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
+import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
 
 
 
 @NgModule({
   declarations: [
-    CursosComponent
+    CursosComponent,
+    CursosDetalheComponent
   ],
   imports: [
     CommonModule
