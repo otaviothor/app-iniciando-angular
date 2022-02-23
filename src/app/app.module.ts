31d3a131a2f5c73eae4eddffefc5a1923822eee0
otavio <otaviosilva2632@gmail.com>
2022-02-22 22:17:22 -0300

    feat: move form to sub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { CursosModule } from './cursos/cursos.module';
 import { PrimeiroComponentComponent } from './primeiro-component/primeiro-component.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { FormsModule } from '@angular/forms';
+import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
-  declarations: [AppComponent, PrimeiroComponentComponent, DataBindingComponent],
-  imports: [BrowserModule, CursosModule, FormsModule],
+  declarations: [
+    AppComponent,
+    PrimeiroComponentComponent,
+    DataBindingComponent,
+  ],
+  imports: [BrowserModule, CursosModule, FormsModule, MeuFormModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/meu-form/meu-form.component.ts
+++ src/app/meu-form/meu-form.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-meu-form',
   templateUrl: './meu-form.component.html',
-  styleUrls: ['./meu-form.component.scss']
+  styleUrls: ['./meu-form.component.scss'],
 })
 export class MeuFormComponent implements OnInit {
+  public inputName: string = 'ABC';
+  public person: { name: string; age: number } = {
+    name: 'def',
+    age: 20,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
