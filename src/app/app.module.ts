496677855930ff56b33d003c93631379e49ba50e
otavio <otaviosilva2632@gmail.com>
2022-03-23 21:33:21 -0300

    feat: move components to subcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CursosModule } from './cursos/cursos.module';
-import { PrimeiroComponentComponent } from './primeiro-component/primeiro-component.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
+import { CursosModule } from './first-classes/cursos/cursos.module';
+import { PrimeiroComponentComponent } from './first-classes/primeiro-component/primeiro-component.component';
+import { DataBindingComponent } from './first-classes/data-binding/data-binding.component';
 import { FormsModule } from '@angular/forms';
-import { MeuFormModule } from './meu-form/meu-form.module';
-import { InputPropertyComponent } from './input-property/input-property.component';
-import { OutputPropertyComponent } from './output-property/output-property.component';
-import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+import { MeuFormModule } from './first-classes/meu-form/meu-form.module';
+import { InputPropertyComponent } from './first-classes/input-property/input-property.component';
+import { OutputPropertyComponent } from './first-classes/output-property/output-property.component';
+import { LifeCycleComponent } from './first-classes/life-cycle/life-cycle.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './first-classes/navbar/navbar.component';
+import { DirectivesComponent } from './first-classes/directives/directives.component';
 import { FirstClassesComponent } from './first-classes/first-classes.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
   declarations: [
